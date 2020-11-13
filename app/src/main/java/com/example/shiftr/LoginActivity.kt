eceec863d6159a51dedc0b54094d90b52f89d4c0
aryanarora180 <aryan.arora180@gmail.com>
2020-11-13 23:24:15 +0530

    Fixed back press bug after OTP verification

--- app/src/main/java/com/example/shiftr/LoginActivity.kt
+++ app/src/main/java/com/example/shiftr/LoginActivity.kt
@@ -2,8 +2,13 @@ package com.example.shiftr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.shiftr.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginActivity : AppCompatActivity(R.layout.activity_login)
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
